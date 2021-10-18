a4ac0484aacc9347751c3764c5eea9c0ece0e237
KASIDATE PHLAIPHUEAK <kasidate.p@mail.kmutt.ac.th>
2021-10-18 21:45:20 +0700

    ver1.2.3.1

--- file/js/filemanager.js
+++ file/js/filemanager.js
@@ -3,33 +3,38 @@ function verifyPassword() {
     var pw = document.getElementById("pswd").value;  
   //check empty password field  
   if(pw == "") {  
-     document.getElementById("message").innerHTML = "**Fill the password please!";  
+     document.getElementById("message").innerHTML = "<pre>**Fill the password please!</pre>";  
      return false;  
   }  
    
  //minimum password length validation  
   if(pw.length < 8) {  
-     document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
+     document.getElementById("message").innerHTML = "<pre>**Password length must be atleast 8 characters</pre>";  
      return false;  
   }  
   
 //maximum length of password validation  
   if(pw.length > 15) {  
-     document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";  
+     document.getElementById("message").innerHTML = "<pre>**Password length must not exceed 15 characters</pre>";  
      return false;  
   } 
   if(pw == "@Kasidate19") {  
-    if(file == "bankdsl2021"){
+    if(file == "test"){
       document.getElementById("message").innerHTML = "<pre> ผลการค้นหาทั้งหมดที่พบของ : " + document.getElementById("file").value  + " คุณสามารถดาวน์โหลดไฟล์ได้ด้วยการคลิกที่ชื่อไฟล์เพื่อทำการดาวน์โหลด</pre>";
       document.getElementById("showcon").innerHTML = '<iframe src="https://kasidate.me/file/display/test.html" width="100%" height="100%"></iframe>';
      return false;  
     }
+    if(file == "bankdsl2021"){
+      document.getElementById("message").innerHTML = "<pre> ผลการค้นหาทั้งหมดที่พบของ : " + document.getElementById("file").value  + " คุณสามารถดาวน์โหลดไฟล์ได้ด้วยการคลิกที่ชื่อไฟล์เพื่อทำการดาวน์โหลด</pre>";
+      document.getElementById("showcon").innerHTML = '<iframe src="https://kasidate.me/file/display/bankdsl2021.html" width="100%" height="100%"></iframe>';
+     return false;  
+    }
     else{
-        document.getElementById("message").innerHTML = "**ไม่พบไฟล์ที่ร้องขอ";  
+        document.getElementById("message").innerHTML = "<pre>**ไม่พบไฟล์ที่ร้องขอ</pre>";  
     }
   }  
   
   else {  
-    document.getElementById("message").innerHTML = "**รหัสผ่านไม่ถูกต้อง";  
+    document.getElementById("message").innerHTML = "<pre>**รหัสผ่านไม่ถูกต้อง</pre>";  
   }  
 }   ;
