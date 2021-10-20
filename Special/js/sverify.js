ea28c598e89baf38796f3e98b7a053595b26769e
KASIDATE PHLAIPHUEAK <kasidate.p@mail.kmutt.ac.th>
2021-10-20 23:29:34 +0700

    special 1.2.0

---
+++ Special/js/sverify.js
@@ -0,0 +1,40 @@
+function sVerify() {
+  var username = document.getElementById("username").value;
+  var pw = document.getElementById("pswd").value;
+  //check empty password field
+  if (pw == "") {
+    document.getElementById("message").innerHTML =
+      "<pre>**Fill the password please!</pre>";
+    return false;
+  }
+
+  //minimum password length validation
+  if (pw.length < 8) {
+    document.getElementById("message").innerHTML =
+      "**Password length must be atleast 8 characters";
+    return false;
+  }
+
+  //maximum length of password validation
+  if (pw.length > 15) {
+    document.getElementById("message").innerHTML =
+      "**Password length must not exceed 15 characters";
+    return false;
+  }
+  if (username == "bank" && pw == "@Kasidate19") {
+    document.getElementById("message").innerHTML =
+    "**Successfully Log In "+ document.getElementById("username").value;
+    window.open("https://kasidate.me/special/2/list");
+    return false;
+  }
+  if (username == "admin" && pw == "12345678") {
+    document.getElementById("message").innerHTML =
+    "**Successfully Log In "+ document.getElementById("username").value;
+    window.open("https://kasidate.me/special/2/list");
+    return false;
+  }
+  else {
+    document.getElementById("message").innerHTML =
+      "ไม่พบข้อมูลในระบบ";
+  }
+}
