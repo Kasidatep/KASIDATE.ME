a01906b2c38aeb534ef6b925731925975c73adb1
KASIDATE PHLAIPHUEAK <kasidate.p@mail.kmutt.ac.th>
2021-11-04 11:52:57 +0700

    add special th

---
+++ Special/project/thailand/js/local-scollbar.js
@@ -0,0 +1,10 @@
+var scroll = 0;
+window.addEventListener('scroll', function(){
+ 
+  if ((document.body.getBoundingClientRect()).top > scrollPos)
+	document.getElementById("local").style.display: none;";
+	else
+	document.getElementById("local").style.display: none;";
+
+	scroll = (document.body.getBoundingClientRect()).top;
+});
