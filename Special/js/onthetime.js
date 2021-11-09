90721c1f373d4ccad93d5e23231b7b62cc9725ef
KASIDATE PHLAIPHUEAK <kasidate.p@mail.kmutt.ac.th>
2021-11-09 13:55:34 +0700

    special

---
+++ Special/js/onthetime.js
@@ -0,0 +1,14 @@
+var currentHour = d.getHours(); //note 0-23
+
+if (currentHour < 20)
+ {
+    document.getElementById("s404").style.display = 'block';
+  }
+else if (currentHour == 13)
+{
+    document.getElementById("s404").style.display = 'block';
+}
+}
+else {
+  
+  }
--- Special/js/sverify.js
+++ Special/js/sverify.js
@@ -1,31 +1,28 @@
 function sVerify() {
-  var username = document.getElementById("username").value;
-  var pw = document.getElementById("password").value;
+  var username = document.getElementById("user").value;
+  var pw = document.getElementById("pswd").value;
   //check empty password field
   if (pw == "") {
     document.getElementById("message").innerHTML =
       "<pre>**Fill the password please!</pre>";
     return false;
   }
-
   //minimum password length validation
   if (pw.length < 8) {
     document.getElementById("message").innerHTML =
       "**Password length must be atleast 8 characters";
     return false;
   }
-
   //maximum length of password validation
   if (pw.length > 15) {
     document.getElementById("message").innerHTML =
       "**Password length must not exceed 15 characters";
     return false;
   }
-   else if (username == "admin" && pw == "12345678") {
+   else if ( pw == "@brtd2021") {
     document.getElementById("message").innerHTML =
     "**Successfully Log In "+ document.getElementById("username").value;
-    window.open("/special/2/list");
-    return false;
+    window.open("./siraphat.html");
   }
 
   else {
