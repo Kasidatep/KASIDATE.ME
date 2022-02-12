49516df24f5edb9af45d71421039862866f61ab6
KASIDATE <kasidate.p@mail.kmutt.ac.th>
2022-02-13 01:01:10 +0700

    basic rss display

---
+++ feed/app.js
@@ -0,0 +1,20 @@
+let url = 'https://blog.kasidate.me/feeds/posts/default';
+const textarea = document.querySelector('#feed-textarea > ul');
+
+const date = new Date();
+document.querySelector('#date').innerHTML = date.toDateString();
+  
+  feednami.load(url)
+  .then(feed => {
+    textarea.value = ''
+    console.log(feed);
+    for(let entry of feed.entries){
+        //create a list element
+        let li = document.createElement('li');
+        //add HTML content to list items
+        li.innerHTML = `<h4><a href="${entry.link}">${entry.title} : ${entry.thumbnail}</a></h4>`;
+        //append HTML content to list 
+        textarea.appendChild(li);
+    }
+  });
+
