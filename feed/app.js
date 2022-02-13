766ade6fcf5053efd5926058af10a897f92d38c6
KASIDATE <kasidate.p@mail.kmutt.ac.th>
2022-02-13 13:57:25 +0700

    rss news bar app blog

--- feed/app.js
+++ feed/app.js
@@ -2,7 +2,7 @@ let url = 'https://blog.kasidate.me/feeds/posts/default?alt=rss';
 const textarea = document.querySelector('#feed-textarea > ul');
 var space = '     ';
 const date = new Date();
-document.querySelector('#date').innerHTML = date.toDateString();
+//document.querySelector('#date').innerHTML = date.toDateString();
 
 function toThaiDateString(date) {
   let monthNames = [
@@ -19,19 +19,13 @@ function toThaiDateString(date) {
   let minutes = date.getMinutes().toString().padStart(2, "0");
   let second = date.getSeconds().toString().padStart(2, "0");
 
-  return `${numOfDay} ${month} ${year} ` +
-    `${hour}:${minutes}:${second} น.`;
+  return `${numOfDay} ${month} ${year} &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;`;
 }
 let today = new Date();
 let date1 = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-let date2 = new Date("September 30, 2020 11:28:00");
-let date3 = new Date("March 12, 1988 00:00:00");
 
+document.querySelector('#display-date').innerHTML = toThaiDateString(date);
 console.log(toThaiDateString(date));
-console.log(toThaiDateString(date2));
-console.log(toThaiDateString(date3));
-
-
 
 feednami.load(url)
   .then(feed => {
@@ -43,7 +37,7 @@ feednami.load(url)
       let li = document.createElement('li');
       //add HTML content to list items
       li.innerHTML = `<div class="categories">${entry.categories} </div>
-      <a href="${entry.link}" class="newsdisplay"> &nbsp; ${entry.title} </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+      <a href="${entry.link}" class="newsdisplay"> &nbsp; ${entry.title} </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
       `;
       //append HTML content to list 
       textarea.appendChild(li);
