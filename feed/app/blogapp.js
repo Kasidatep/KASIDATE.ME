dec51a6755a2e8033c45e2cd8bd7b03acb164e0a
KASIDATE <kasidate.p@mail.kmutt.ac.th>
2022-02-20 11:26:09 +0700

    1.5.0 bug 1

--- feed/app/blogapp.js
+++ feed/app/blogapp.js
@@ -16,7 +16,7 @@ feednami.load(blogurl)
       <div class="uk-card uk-card-default uk-card-body uk-ext-margin15 uk-contmar uk-animation-toggle " tabindex="0" >
       <img src="${entry.image.url}" class="uk-ext-margin10"> 
       <span class="uk-label uk-ext-z1 uk-ext-textmargin3 uk-text-bold uk-animation-shake"><span uk-icon="tag" > </span> ${entry.categories}</span> 
-       <a href="${entry.link}" class="uk-ext-z1 uk-link-heading uk-animation-scale-up">  ${feed.meta.title} &nbsp; ${entry.title} >>>&nbsp; &nbsp; 
+       <a href="${entry.link}" class="uk-ext-z1 uk-link-heading uk-animation-scale-up">  ${feed.meta.title} &nbsp; ${entry.title} &nbsp; &nbsp; 
        </a> 
       
       </div>`;
